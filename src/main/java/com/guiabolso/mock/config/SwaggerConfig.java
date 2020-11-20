ea2be37081c8dc897dceb9f6c6ef8ff933b857bf
Eduardo Lodi Marzano <lodi001@uol.com.br>
2020-11-20 19:57:49 -0300

    Updated SwaggerConfig

--- src/main/java/com/guiabolso/mock/config/SwaggerConfig.java
+++ src/main/java/com/guiabolso/mock/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package com.guiabolso.mock.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +20,18 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.guiabolso.mock.controller"))
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Mock API")
+                .description("Sample Mock REST API that simulates transactions based on id, month and year")
+                .version("1.0.0")
+                .license("MIT_License")
+                .licenseUrl("https://mit-license.org")
+                .build();
+
     }
 }
